893cb489b1cecddba7cfb981046385835ccce139
atulyagh03 <atulyagh03@github.com>
2021-08-29 18:57:01 +0530

    Global parameters defined

--- p2p-sim.py
+++ p2p-sim.py
@@ -5,12 +5,15 @@ import numpy as np
 n = 6               #number of nodes in P2P network
 z = 0.3             #probability that a node is slow
 p_edge = 0.3        #probability with which edge is drawn between two nodes
-peers = {}          #will store adjacency lists, it is a dictionary of lists
 seed = 102          #seed for random functions
-
 
 np.random.seed(seed)
 
+peers = {}          #will store adjacency lists, it is a dictionary of lists
+rho = {}            #stores speed of light propagation delay, accessed as rho[i,j]
+c = {}              #stores link speed, used as c[i,j]
+d_mean = {}         #stores mean of exponential dist for queuing delay, used as d_mean[i,j]
+nodes = []          #stores nodes of the network
 
 def coin_flip(p):
     """Generates 1 with p probabilty"""
@@ -27,22 +30,6 @@ def connect_dfs(i, visited):
         if(not visited[j]):
             connect_dfs(j, visited)
 
-def connect_graph():
-    """
-    Calls dfs and connects dfs roots
-    """
-    roots = []
-    visited = {}
-    for i in range(n):
-        visited[i] = False
-    for i in range(n):
-        if(not visited[i]):
-            roots.append(i)
-            connect_dfs(i, visited)
-    for i in range(len(roots)-1):
-        peers[roots[i]].append(roots[i+1])
-        peers[roots[i+1]].append(roots[i])
-
 def gen_graph():
     """
     Samples the random graph for P2P network
@@ -57,7 +44,17 @@ def gen_graph():
             if(coin_flip(p_edge)):
                 peers[i].append(j)
                 peers[j].append(i)
-    connect_graph()
+    roots = []
+    visited = {}
+    for i in range(n):
+        visited[i] = False
+    for i in range(n):
+        if(not visited[i]):
+            roots.append(i)
+            connect_dfs(i, visited)
+    for i in range(len(roots)-1):
+        peers[roots[i]].append(roots[i+1])
+        peers[roots[i+1]].append(roots[i])
 
 def print_graph():
     """Prints adjacency list"""
@@ -66,4 +63,23 @@ def print_graph():
         for j in peers[i]:
             print(j, ' ', end='')
         print()
+
+def init_global_values():
+    for i in range(n):
+        for j in peers[i]:
+            rho[i,j] = (10 + 490*np.random.random())/1000
+            if(nodes[i].is_fast and nodes[j].is_fast):
+                c[i,j] = 100
+            else:
+                c[i,j] = 5
+            d_mean[i,j] = 96/([c[i,j]]*1000)
+
+def get_latency(i, j, m_size):
+    """
+    Calculate latency in sending message of size m_size from note i to j
+    m_size is in kbits
+    """
+    latency = rho[i,j] + m_size/(c[i,j]*1000)+ np.random.exponential(d_mean[i,j])
+    return latency
+    
 
